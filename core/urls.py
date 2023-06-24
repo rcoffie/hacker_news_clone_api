27fd69422c51b8ff32f4d7889e045b2fee2a979d
Robert Coffie <rcoffie22@yahoo.com>
2023-06-24 01:22:51 +0000

    creating custome user

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('story/',include('story.urls')),
+    path('user_engine/',include('user_engine.urls')),
 ]
